054ae467e1a329b406ce2d10c58b72b2af58f174
Lijun <gan.lijun.glj@gmail.com>
2021-03-17 19:12:08 -0400

    create a demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+demoUser = User.create(username: "AmazenShopper", email: "amazen@gmail.com", password: "amazenLover" )
